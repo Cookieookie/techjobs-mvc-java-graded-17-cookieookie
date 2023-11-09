e2e58e11a0d35abc4d944e90a091d2c343b3a9a7
Cookie Van <cookievan03@gmail.com>
2023-11-08 18:39:46 -0600

    fixed to pass test 2 per TA Madison's help

--- src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
@@ -29,7 +29,7 @@ public class SearchController {
     @PostMapping("results")
     public String displaySearchResults(Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
         ArrayList<Job> jobs;
-        if (searchTerm.equals("all") || searchTerm.equals("")) {
+        if (searchTerm.equals("all") || searchTerm.equals("") || searchTerm == null) {
             jobs = jobs = JobData.findAll();
             model.addAttribute("title", "All Jobs");
 
